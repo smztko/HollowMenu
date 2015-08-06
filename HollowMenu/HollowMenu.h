aa743574d7b8a729eb1879572fe58d897f76d88a
SHIMIZU Takeo <takeo@pwater.org>
2015-08-06 23:11:56 +0900

    Initial Commit

---
+++ HollowMenu/HollowMenu.h
@@ -0,0 +1,19 @@
+//
+//  HollowMenu.h
+//  HollowMenu
+//
+//  Created by takeo on 8/6/15.
+//  Copyright (c) 2015 Pwater.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HollowMenu.
+FOUNDATION_EXPORT double HollowMenuVersionNumber;
+
+//! Project version string for HollowMenu.
+FOUNDATION_EXPORT const unsigned char HollowMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HollowMenu/PublicHeader.h>
+
+
